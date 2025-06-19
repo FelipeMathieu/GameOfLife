553740d26e17a5d4d810eba29a18723cad96630a
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-06-19 13:33:11 -0300

    Add tests
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/components/hooks/__tests__/fill-creature.test.ts
+++ src/components/hooks/__tests__/fill-creature.test.ts
@@ -1,3 +1,55 @@
-import { describe } from "vitest";
+import { renderHook } from "@testing-library/react";
+import { useRef } from "react";
+import { describe, expect, it, vi } from "vitest";
+import { useFillCreature } from "../fill-creature";
+import { Creature } from "../../../common/models";
+import type { TCreatures } from "../../../common/types";
+import { values } from "lodash";
 
-describe("Fill creature", () => {});
+const mockCells: TCreatures = {
+  "0,0": new Creature(0, 0, true),
+  "0,1": new Creature(0, 1, true),
+  "1,0": new Creature(1, 0, true),
+  "1,1": new Creature(1, 1, true),
+};
+
+vi.mock("../../../core/store", () => ({
+  useCreaturesStore: {
+    getState: () => ({
+      cells: mockCells,
+      subscribe: vi.fn(),
+      destroy: vi.fn(),
+    }),
+  },
+}));
+
+describe("useFillCreature", () => {
+  it("should call fillRect for each creature", () => {
+    const fillRect = vi.fn();
+    const strokeRect = vi.fn();
+    const creaturesLength = values(mockCells).length;
+
+    const canvasMock = document.createElement("canvas");
+
+    vi.spyOn(canvasMock, "getContext").mockReturnValue({
+      fillRect,
+      strokeRect,
+      fillStyle: "",
+      strokeStyle: "",
+    } as unknown as CanvasRenderingContext2D);
+
+    const { result: refResult } = renderHook(() => {
+      return {
+        canvasRef: useRef<HTMLCanvasElement>(canvasMock),
+        drawnStatesRef: useRef<Map<string, boolean>>(new Map()),
+      };
+    });
+
+    const { canvasRef, drawnStatesRef } = refResult.current;
+
+    renderHook(() => useFillCreature(canvasRef, drawnStatesRef));
+
+    expect(fillRect).toHaveBeenCalledTimes(creaturesLength);
+    expect(strokeRect).toHaveBeenCalledTimes(creaturesLength);
+  });
+});
