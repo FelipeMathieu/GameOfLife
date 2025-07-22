9af97a7e2962693a3fdc52d8c39f301245f3c3ae
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-07-22 11:08:02 -0300

    Add module federation
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ENV = process.env.VITE_ENV;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      dev: ENV === "DEV",
+      name: "reactGameOfLife",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+        "react-dom": {
+          singleton: true,
+        },
+        antd: {
+          singleton: true,
+        },
+      },
+    }),
+  ],
   server: {
-    port: 3000,
+    port: 3001,
     allowedHosts: true,
     open: true,
+  },
+  build: {
+    target: "esnext",
   },
 });
